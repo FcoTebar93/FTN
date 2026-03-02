906b94d78c551cbc6c998f165c67e82077c20850
FcoTebar93 <fcotebar93@gmail.com>
2026-03-02 16:39:11 +0100

    chore(package): Update devDependencies and add CancellationSignal type

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -5,6 +5,12 @@ export type WorkerId = string;
 export type EventId = string;
 export type Version = number;
 
+export type CancellationSignal = {
+    readonly aborted: boolean;
+    addEventListener(type: "abort", listener: () => void): void;
+    removeEventListener(type: "abort", listener: () => void): void;
+};
+
 export interface Timestamped {
     timestamp: string;
 }
